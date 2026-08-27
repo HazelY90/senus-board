0b9cb1af83cd43757dec5b1472cdb3dfa441a8ea
HazelY <205923580+HazelY90@users.noreply.github.com>
2026-08-27 21:01:45 +0100

    Set up backend project

---
+++ backend/src/main/java/com/hazely/senusboard/SenusBoardApplication.java
@@ -0,0 +1,13 @@
+package com.hazely.senusboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenusBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SenusBoardApplication.class, args);
+    }
+
+}
